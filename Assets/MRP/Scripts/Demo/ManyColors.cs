b6b90f5695810d7982ecf6340d928db8bdf07b85
朱磊 <zhulei@xmfunny.com>
2021-01-06 19:45:03 +0800

    <misc> breaks SRP Batcher

---
+++ Assets/MRP/Scripts/Demo/ManyColors.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class ManyColors : MonoBehaviour {
+    static int baseColorID = Shader.PropertyToID("_BaseColor");
+    static MaterialPropertyBlock materialPropertyBlock;
+    [SerializeField]
+    Color baseColor = Color.white;
+    private void Awake() {
+        OnValidate();
+    }
+
+    private void OnValidate() {
+      
+        if(materialPropertyBlock == null) {
+            materialPropertyBlock = new MaterialPropertyBlock();
+        }
+        materialPropertyBlock.SetColor(baseColorID, baseColor);
+        GetComponent<Renderer>().SetPropertyBlock(materialPropertyBlock);
+    }
+}
