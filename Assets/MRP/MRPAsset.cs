6aed25ca6b6b4d788df9529fef977954a1e1660a
Zhulei <zhulei@xmfunny.com>
2021-06-30 21:53:39 +0800

    <feature> render skybox

--- Assets/MRP/MRPAsset.cs
+++ Assets/MRP/MRPAsset.cs
@@ -6,7 +6,6 @@ using UnityEngine.Rendering;
 [CreateAssetMenu(menuName = "Rendering / Miles Render Pipeline Asset")]
 public class MRPAsset : RenderPipelineAsset {
     protected override RenderPipeline CreatePipeline() {
-        Debug.Log("one time rendering");
         return new MilesRenderPipeline();
     }
 }
--- Assets/MRP/MilesRenderPipeline.cs
+++ Assets/MRP/MilesRenderPipeline.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 public class MilesRenderPipeline : RenderPipeline {
+    SimpleRenderer simpleRenderer = new SimpleRenderer();
     protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
-        
+        foreach(Camera camera in cameras) {
+            simpleRenderer.Render(context, camera);
+        }
     }
 }
