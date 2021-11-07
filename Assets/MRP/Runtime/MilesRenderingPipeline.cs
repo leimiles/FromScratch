0f27444124de37a6d43250aae97c6c3fe9d58902
leimiles <leimiles_18@hotmail.com>
2021-11-07 16:38:53 +0800

    <feature> create rp asset

---
+++ Assets/MRP/Runtime/MilesRenderingPipeline.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MilesRenderingPipeline : RenderPipelineAsset {
+    protected override RenderPipeline CreatePipeline() {
+        return null;
+    }
+}
