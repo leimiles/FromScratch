f6af88d299aab3e33241f7e9aa6540efeb69e103
ZhuLei <zhulei@xmfunny.com>
2022-03-01 15:15:55 +0800

    <feature> update prj

---
+++ Assets/Scripts/JobTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+
+public class JobTest : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
