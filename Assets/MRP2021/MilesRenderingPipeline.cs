cca9d7af54fef72b996527f0a9d258b7369b4675
ZhuLei <zhulei@xmfunny.com>
2022-05-23 19:37:14 +0800

    <feature> update make urp editable

---
+++ Assets/MRP2021/MilesRenderingPipeline.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace MRP {
+    public sealed partial class MilesRenderingPipeline : RenderPipeline {
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
